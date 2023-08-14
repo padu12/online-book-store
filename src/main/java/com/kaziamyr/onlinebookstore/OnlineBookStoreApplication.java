317a8151c5ded3fdf9d4de18df90861911a9562c
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-14 15:01:21 +0300

    removed unnecessary field from OnlineBookStoreApplication.java

--- src/main/java/com/kaziamyr/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/kaziamyr/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,17 +1,10 @@
 package com.kaziamyr.onlinebookstore;
 
-import com.kaziamyr.onlinebookstore.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-    private final BookService bookService;
-
-    public OnlineBookStoreApplication(BookService bookService) {
-        this.bookService = bookService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
