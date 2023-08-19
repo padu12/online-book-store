f9f6682971b763de2fcf5412ff1e98822be0bb97
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-19 19:20:46 +0300

    add @Valid to the update method in BookController

--- src/main/java/com/kaziamyr/onlinebookstore/controller/BookController.java
+++ src/main/java/com/kaziamyr/onlinebookstore/controller/BookController.java
@@ -47,7 +47,8 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public BookDto updateById(@PathVariable Long id, @RequestBody CreateBookRequestDto bookDto) {
+    public BookDto updateById(@PathVariable Long id,
+                              @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.updateById(id, bookDto);
     }
 
