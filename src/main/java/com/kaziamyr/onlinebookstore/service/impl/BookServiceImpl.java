21e753a0203d288b556387b0e521dd78ce1489c2
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-21 10:27:46 +0300

    remove redundant new line

--- src/main/java/com/kaziamyr/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/kaziamyr/onlinebookstore/service/impl/BookServiceImpl.java
@@ -30,8 +30,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<BookDto> findAll(Pageable pageable) {
-        return bookRepository.findAll(
-                        pageable).stream()
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
