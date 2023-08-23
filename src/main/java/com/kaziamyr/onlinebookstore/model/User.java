45b11884aecd84efa7270ebaf9adbd548d8e8af6
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-23 16:56:54 +0300

    finish creating User and Role entity

--- src/main/java/com/kaziamyr/onlinebookstore/model/User.java
+++ src/main/java/com/kaziamyr/onlinebookstore/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
@@ -28,5 +29,6 @@ public class User {
     @Column(nullable = false)
     private String lastName;
     private String shoppingAddress;
-    private
+    @ManyToOne
+    private Role role;
 }
