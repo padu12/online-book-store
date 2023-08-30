3d160b8342264c444ee21dba17732907f4153b2b
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-30 18:16:41 +0300

    add RegistrationException

---
+++ src/main/java/com/kaziamyr/onlinebookstore/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package com.kaziamyr.onlinebookstore.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
