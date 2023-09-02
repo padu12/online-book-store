2c607ad541e586662b5bb6caea0eb2b1136c44c0
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-02 18:42:20 +0300

    add BookMaper and CategoryMapper

--- src/main/java/com/kaziamyr/onlinebookstore/dto/book/BookDtoWithoutCategoryIds.java
+++ src/main/java/com/kaziamyr/onlinebookstore/dto/book/BookDtoWithoutCategoryIds.java
@@ -1,16 +1,13 @@
 package com.kaziamyr.onlinebookstore.dto.book;
 
-import com.kaziamyr.onlinebookstore.model.Category;
 import java.math.BigDecimal;
-import java.util.Set;
 import lombok.Data;
 
 @Data
-public class BookDto {
+public class BookDtoWithoutCategoryIds {
     private Long id;
     private String title;
     private String author;
-    private Set<Category> categories;
     private String isbn;
     private BigDecimal price;
     private String description;
--- src/main/java/com/kaziamyr/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/kaziamyr/onlinebookstore/mapper/BookMapper.java
@@ -2,13 +2,27 @@ package com.kaziamyr.onlinebookstore.mapper;
 
 import com.kaziamyr.onlinebookstore.config.MapperConfig;
 import com.kaziamyr.onlinebookstore.dto.book.BookDto;
+import com.kaziamyr.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import com.kaziamyr.onlinebookstore.dto.book.CreateBookRequestDto;
 import com.kaziamyr.onlinebookstore.model.Book;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+    //todo (HINT: BookDtoWithoutCategoryIds class could be used as a response class for @GetMapping("/{id}/books") endpoint)
+
+    @AfterMapping
+        // todo should be default and check implementation in target
+    void setCategoryIds(@MappingTarget BookDto bookDto, Book book);
+
+    @Named("bookFromId")
+    Book bookFromId(Long id); // todo should be default and check implementation in target
 }
