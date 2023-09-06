a1501bdd5c9d288d8f36f6a37dbafec248efcd21
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-06 14:57:00 +0300

    add codestyle fixes

--- src/main/java/com/kaziamyr/onlinebookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/kaziamyr/onlinebookstore/exception/CustomGlobalExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
