a97d5ab53d93d69d5f92d861b22fd7b50f5ed78f
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-07 12:42:56 +0300

    change tag in BookController

--- src/main/java/com/kaziamyr/onlinebookstore/controller/BookController.java
+++ src/main/java/com/kaziamyr/onlinebookstore/controller/BookController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Book management", description = "Endpoints for managing books")
+@Tag(name = "Book management 'addchanges'", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
