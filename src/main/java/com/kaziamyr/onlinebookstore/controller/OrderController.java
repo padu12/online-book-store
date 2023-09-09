579c4fe8c812540f8973409e1d909df008e85938
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-09 12:25:14 +0300

    add swagger to OrderController

--- src/main/java/com/kaziamyr/onlinebookstore/controller/OrderController.java
+++ src/main/java/com/kaziamyr/onlinebookstore/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.kaziamyr.onlinebookstore.controller;
 import com.kaziamyr.onlinebookstore.dto.OrderDto;
 import com.kaziamyr.onlinebookstore.dto.OrderItemDto;
 import com.kaziamyr.onlinebookstore.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Order management", description = "Endpoints for managing orders")
+@Tag(name = "Order management", description = "Endpoints for managing orders and their items")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/order")
@@ -25,19 +26,21 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping
+    @Operation(summary = "Place user's order according items in shopping cart")
     public OrderDto place(@RequestBody Map<String, String> requestBody) {
         return orderService.create(requestBody);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping
+    @Operation(summary = "Retrieve user's order history")
     public List<OrderDto> retrieveUsersHistory() {
         return orderService.findAllByUser();
     }
 
-
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PatchMapping("/{id}")
+    @Operation(summary = "Update status of order by its id")
     public OrderDto updateStatus(
             @PathVariable Long id,
             @RequestBody Map<String, String> requestBody
@@ -47,12 +50,14 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/{orderId}/items")
+    @Operation(summary = "Retrieve all order items from a specific order")
     public List<OrderItemDto> retrieveAllOrderItemsByOrderId(@PathVariable Long orderId) {
         return orderService.getOrderItemsByOrderId(orderId);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/{orderId}/items/{itemId}")
+    @Operation(summary = "Retrieve a specific item from a specific order")
     public OrderItemDto retrieveOrderItemById(
             @PathVariable Long orderId,
             @PathVariable Long itemId
