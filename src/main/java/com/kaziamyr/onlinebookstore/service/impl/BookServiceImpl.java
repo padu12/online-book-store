baf2926c1aa77d962a2b7f5c98bc0a354b15fcdc
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-10 11:22:32 +0300

    change using getCategoryById() to getReferenceById()

--- src/main/java/com/kaziamyr/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/kaziamyr/onlinebookstore/service/impl/BookServiceImpl.java
@@ -31,7 +31,7 @@ public class BookServiceImpl implements BookService {
         Book book = bookMapper.toModel(requestDto);
         book.setCategories(
                 Stream.of(requestDto.getCategoryIds())
-                        .map(categoryRepository::getCategoryById)
+                        .map(categoryRepository::getReferenceById)
                         .collect(Collectors.toSet())
         );
         return bookMapper.toDto(bookRepository.save(book));
