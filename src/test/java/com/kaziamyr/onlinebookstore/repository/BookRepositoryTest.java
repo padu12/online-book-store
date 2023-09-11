a4fc9fcd42192efcb4963cf09d47ccfbfdec77c0
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-11 18:05:37 +0300

    preparation to test repository layer

---
+++ src/test/java/com/kaziamyr/onlinebookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.kaziamyr.onlinebookstore.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Test findBookById()")
+    void findBookById_validId_returnOptionalOfBook() {
+
+    }
+}
