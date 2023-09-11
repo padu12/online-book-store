4d40f3488d0aa8a1dd81b119631d6df169707f06
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-09-11 15:18:43 +0300

    add tests for CategoryServiceImpl class

--- src/main/java/com/kaziamyr/onlinebookstore/dto/category/SaveCategoryRequestDto.java
+++ src/main/java/com/kaziamyr/onlinebookstore/dto/category/SaveCategoryRequestDto.java
@@ -3,8 +3,10 @@ package com.kaziamyr.onlinebookstore.dto.category;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class SaveCategoryRequestDto {
     @NotNull
     @Size(min = 1, max = 100)
